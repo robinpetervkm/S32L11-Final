438bc9a95d205e7cd99b074253e61035acce7101
Robinclear <robin.peter.vkm@gmail.com>
2020-04-02 18:01:57 -0300

    Final Working

--- src/com/norha/app/DAO/ProductDAOImpl.java
+++ src/com/norha/app/DAO/ProductDAOImpl.java
@@ -8,10 +8,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.norha.app.entity.Product;
-
-
+@Repository
 public class ProductDAOImpl implements ProductDAO {
 
 	@Autowired
